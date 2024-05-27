8f5d8a019470d42b966ecaaa51d5001ebc7957ea
AppVeyor <ci@appveyor.com>
2024-05-27 19:13:06 +0000

    Updates

--- main.js
+++ main.js
@@ -4,10 +4,10 @@
 /***/ 742:
 /***/ (() => {
 
-const textEl = document.querySelector('.text');
-const buttonEl = document.querySelector('.btn-сollapse');
-buttonEl.addEventListener('click', () => {
-  textEl.classList.toggle('open');
+const textEl = document.querySelector(".text");
+const buttonEl = document.querySelector(".btn-сollapse");
+buttonEl.addEventListener("click", () => {
+  textEl.classList.toggle("open");
 });
 
 /***/ }),
@@ -22,26 +22,26 @@ class Like {
     this.init();
   }
   init() {
-    const likeButton = document.createElement('button');
-    likeButton.classList.add('like-button');
+    const likeButton = document.createElement("button");
+    likeButton.classList.add("like-button");
     likeButton.textContent = "Like";
-    const likeIcon = document.createElement('img');
+    const likeIcon = document.createElement("img");
     likeIcon.src = "./img/heart.png";
-    likeIcon.classList.add('like-icon');
+    likeIcon.classList.add("like-icon");
     likeButton.appendChild(likeIcon);
     this.container.appendChild(likeButton);
-    const buttonLike = document.querySelector('.like-button');
-    buttonLike.addEventListener('click', () => {
+    const buttonLike = document.querySelector(".like-button");
+    buttonLike.addEventListener("click", () => {
       this.animation(buttonLike);
     });
   }
   animation(buttonLike) {
-    const likeIcon = this.container.querySelector('.like-icon');
+    const likeIcon = this.container.querySelector(".like-icon");
     const likeElement = likeIcon.cloneNode(true);
-    likeElement.classList.add('like-animation');
+    likeElement.classList.add("like-animation");
     likeElement.style.animationName = this.getRandomElement(this.variants);
     buttonLike.appendChild(likeElement);
-    likeElement.addEventListener('animationend', () => {
+    likeElement.addEventListener("animationend", () => {
       likeElement.remove();
     });
   }
@@ -62,36 +62,36 @@ class Widget {
     this.init();
   }
   init() {
-    const widgetWrap = document.createElement('div');
-    widgetWrap.classList.add('widget-wrap');
-    const btnWidgetOpen = document.createElement('button');
-    btnWidgetOpen.classList.add('btn-widget-open');
-    btnWidgetOpen.setAttribute('type', 'button');
+    const widgetWrap = document.createElement("div");
+    widgetWrap.classList.add("widget-wrap");
+    const btnWidgetOpen = document.createElement("button");
+    btnWidgetOpen.classList.add("btn-widget-open");
+    btnWidgetOpen.setAttribute("type", "button");
     widgetWrap.appendChild(btnWidgetOpen);
     this.container.appendChild(widgetWrap);
     this.createWidget();
     this.addEventListeners();
   }
   createWidget() {
-    const widgetWrap = this.container.querySelector('.widget-wrap');
-    const widget = document.createElement('div');
-    widget.classList.add('widget');
-    const widgetTitle = document.createElement('div');
-    widgetTitle.classList.add('widget-title');
-    const h2 = document.createElement('h2');
-    h2.textContent = 'Напишите нам';
+    const widgetWrap = this.container.querySelector(".widget-wrap");
+    const widget = document.createElement("div");
+    widget.classList.add("widget");
+    const widgetTitle = document.createElement("div");
+    widgetTitle.classList.add("widget-title");
+    const h2 = document.createElement("h2");
+    h2.textContent = "Напишите нам";
     widgetTitle.appendChild(h2);
-    const widgetContent = document.createElement('textarea');
-    widgetContent.classList.add('widget-content');
-    const widgetButton = document.createElement('div');
-    widgetButton.classList.add('widget-button');
-    const btn = document.createElement('button');
-    btn.classList.add('btn');
-    btn.setAttribute('type', 'submit');
-    btn.textContent = 'Отправить';
+    const widgetContent = document.createElement("textarea");
+    widgetContent.classList.add("widget-content");
+    const widgetButton = document.createElement("div");
+    widgetButton.classList.add("widget-button");
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+    btn.setAttribute("type", "submit");
+    btn.textContent = "Отправить";
     widgetButton.appendChild(btn);
-    const btnWidgetClose = document.createElement('span');
-    btnWidgetClose.classList.add('span-close');
+    const btnWidgetClose = document.createElement("span");
+    btnWidgetClose.classList.add("span-close");
     widget.appendChild(btnWidgetClose);
     widget.appendChild(widgetTitle);
     widget.appendChild(widgetContent);
@@ -99,20 +99,20 @@ class Widget {
     widgetWrap.insertBefore(widget, widgetWrap.firstChild);
   }
   addEventListeners() {
-    const btnWidgetOpen = this.container.querySelector('.btn-widget-open');
-    const widget = this.container.querySelector('.widget');
-    btnWidgetOpen.addEventListener('click', () => {
-      widget.classList.add('open-widget');
-      btnWidgetOpen.classList.add('btn-widget-open-hidden');
+    const btnWidgetOpen = this.container.querySelector(".btn-widget-open");
+    const widget = this.container.querySelector(".widget");
+    btnWidgetOpen.addEventListener("click", () => {
+      widget.classList.add("open-widget");
+      btnWidgetOpen.classList.add("btn-widget-open-hidden");
     });
-    const btnWidgetClose = this.container.querySelector('.span-close');
-    btnWidgetClose.addEventListener('click', e => {
-      widget.classList.remove('open-widget');
-      widget.classList.add('close-widget');
-      widget.addEventListener('animationend', () => {
-        widget.classList.remove('close-widget');
+    const btnWidgetClose = this.container.querySelector(".span-close");
+    btnWidgetClose.addEventListener("click", () => {
+      widget.classList.remove("open-widget");
+      widget.classList.add("close-widget");
+      widget.addEventListener("animationend", () => {
+        widget.classList.remove("close-widget");
       });
-      btnWidgetOpen.classList.remove('btn-widget-open-hidden');
+      btnWidgetOpen.classList.remove("btn-widget-open-hidden");
     });
   }
 }
